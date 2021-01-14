a92fc88814d79d5ba641d80f730d904291554e84
ytyaru <yttry0@gmail.com>
2021-01-14 20:07:53 +0900

    add

--- src/1/main.js
+++ src/1/main.js
@@ -1,4 +1,25 @@
 window.addEventListener('load', (event) => {
-    let sub = new Sub();
-    sub.show();
+    window.addEventListener('click', async() => {
+        async function writeFile(fileHandle, contents) {
+          const writable = await fileHandle.createWritable();
+          await writable.write(contents);
+          await writable.close();
+        }
+        const saveFileOptions = {
+          types: [
+            {
+              description: "Text Files",
+              accept: {
+                "text/plain": [".txt"],
+              },
+            },
+          ],
+        };
+        const textContent = "書き込みたい内容";
+        const handle = await window.showSaveFilePicker(saveFileOptions);
+        await writeFile(handle, textContent);
+        console.log('書込完了');
+    });
+
 });
+
